1307dceab2534b67cb5c59df45caae3734a1a5b0
Kevin Alexander <kalex.bica@gmail.com>
2020-04-07 17:38:24 -0600

    setup inital react directory with test components

--- alyx/src/App.js
+++ alyx/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './Components/Card'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <nav>
+        <img id='react' src={logo} className="App-logo" alt="logo" />
+      </nav>
+      <body>
+        <p>This is where some things will go, others will not.</p>
+        <div id='data'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </body>
+    </main>
   );
 }
 
